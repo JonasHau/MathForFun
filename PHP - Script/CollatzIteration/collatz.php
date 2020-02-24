500ada2720d790978231701d02e4080359e41974
Frosted <35954302+JonasHau@users.noreply.github.com>
2020-02-24 18:38:11 +0100

    Codacy should be feeling better now

--- PHP - Script/CollatzIteration/collatz.php
+++ PHP - Script/CollatzIteration/collatz.php
@@ -1,22 +1,25 @@
 <?php
 while(true)
 {
-	echo("Enter a number ? ");
+	print_r("Enter a number ? ");
 	$input = fgets(STDIN);
 	$counter = 0;
 	
 	$number = intval($input);
 	
-	if($number <= 0) { echo("enter a number greater than 0 please.\n\n"); }
+	if($number <= 0) { print_r("enter a number greater than 0 please.\n\n"); }
 	else
 	{
 		while($number != 1)
 		{
-			$number = $number % 2 == 0 ? $number / 2 : (3 * $number) + 1;
+			$number = $number%2 == 0 ? $number/2 : 3*$number+1;
 			
 			$counter++;
-			echo($number."\t");
+			if (is_numeric($number)) { print_R($number."\t"); }
 		}
-		echo("\n".intval($input)." took ".$counter." iteration(s).\n\n");
+		
+		if (is_numeric($number)) { 
+			print_r("\n".intval($input)." took ".$counter." iteration(s).\n\n");
+		}
 	}
 }
